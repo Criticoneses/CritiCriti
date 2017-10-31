15c86ba5dcfe5f96ecaacfdb70228a1255960c25
VicDance <VicDance@VicDance>
2017-10-31 12:37:37 +0100

    Modificado

---
+++ ClasesGenericas/src/Chocolatina.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author VicDance
+ */
+public class Chocolatina {
+    
+    private String marca;
+    
+    public String getMarca(){
+        
+        return marca;
+    }
+    
+    public void setMarca(String marca){
+        
+        this.marca=marca;
+    }
+
+    Chocolatina(String marca) {
+        
+        super();
+        this.marca=marca;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
